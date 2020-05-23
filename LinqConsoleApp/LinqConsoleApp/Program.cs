47a4abc8c0b6d6346b52cb2daea78a03cc199cf2
s16520 <s16520@pjwstk.edu.pl>
2020-05-23 17:35:15 +0200

    LINQ, zadanie 8/9

---
+++ LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqConsoleApp
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            d.Przyklad12();
+        }
+    }
+}
